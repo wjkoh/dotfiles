3b762dea849315b54e20bdac19d29f04b97668e5
Woojong Koh <wjngkoh@gmail.com>
2012-09-10 14:59:46 -0700

    Add osxkeychain to .gitconfig and add some libraries to bootstrap_mac.sh

--- bootstrap_mac.sh
+++ bootstrap_mac.sh
@@ -4,8 +4,21 @@ sudo port install autojump
 sudo port install ccache
 sudo port install ctags
 sudo port install gcc47
+sudo port install tig
+
 sudo port install tmux
+git clone https://github.com/ChrisJohnsen/tmux-MacOSX-pasteboard.git
+cd tmux-MacOSX-pasteboard
+make reattach-to-user-namespace && sudo cp reattach-to-user-namespace /opt/local/bin
+cd ..
+rm -rf tmux-MacOSX-pasteboard
+
 sudo port install weechat +aspell +perl +python +tls
 sudo port install wget
-#sudo port install p5-app-ack
-cpan App::Ack
+sudo cpan App::Ack
+
+sudo port install glew
+sudo port install jpeg
+sudo port install freetype
+
+# install MacVim
