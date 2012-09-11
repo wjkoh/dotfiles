ae7c3a18c998dfea63bde4f74e28cd7de38bf8be
Woojong Koh <wjngkoh@gmail.com>
2012-09-10 23:39:06 -0700

    Clean up bootstrap_mac.sh

--- bootstrap_mac.sh
+++ bootstrap_mac.sh
@@ -3,30 +3,6 @@
 xcodebuild -license
 sudo xcodebuild -license
 
-sudo port selfupdate
-sudo port install autojump
-sudo port install ccache
-sudo port install ctags
-sudo port install gcc47
-sudo port install tig
-
-sudo port install tmux
-git clone https://github.com/ChrisJohnsen/tmux-MacOSX-pasteboard.git
-cd tmux-MacOSX-pasteboard
-make reattach-to-user-namespace && sudo cp reattach-to-user-namespace /opt/local/bin
-cd ..
-rm -rf tmux-MacOSX-pasteboard
-
-sudo port install weechat +aspell +perl +python +tls
-sudo port install wget
-sudo cpan App::Ack
-
-sudo port install glew
-sudo port install jpeg
-sudo port install freetype
-
-# install MacVim
-
 # Back to My Mac (SSH)
 HOSTNAME=`hostname -s`
 HOSTNAME_CLEANED=${HOSTNAME//-/_}
@@ -37,3 +13,32 @@ HOSTNAMES_FILE="$HOME/Dropbox/Mac Sync/.hostnames"
 if ! grep "$EXPORT_STMT" "$HOSTNAMES_FILE" > /dev/null; then
     echo $EXPORT_STMT >> $HOSTNAMES_FILE
 fi
+
+# Update MacPorts
+sudo port selfupdate
+sudo port upgrade outdated
+
+# Install compilers and utilities
+sudo port install autojump
+sudo port install ccache
+sudo port install ctags
+sudo port install gcc47
+sudo port install tig
+sudo port install weechat +aspell +perl +python +tls
+sudo port install wget
+sudo cpan App::Ack
+
+# tmux
+sudo port install tmux
+git clone https://github.com/ChrisJohnsen/tmux-MacOSX-pasteboard.git
+cd tmux-MacOSX-pasteboard
+make reattach-to-user-namespace && sudo cp reattach-to-user-namespace /opt/local/bin
+cd ..
+rm -rf tmux-MacOSX-pasteboard
+
+# Install libraries
+sudo port install glew
+sudo port install jpeg
+sudo port install freetype
+
+# Install MacVim
