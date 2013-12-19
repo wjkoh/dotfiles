c954aad0981995467f01fd69b7b332acad27fe6f
Woojong Koh <wjngkoh@gmail.com>
2013-12-18 18:23:47 -0800

    Update subrepos

--- bin/update_subrepos.sh
+++ bin/update_subrepos.sh
@@ -3,7 +3,6 @@
 # Git
 hg onsub --ignore-errors "git checkout master"
 hg onsub --ignore-errors "git pull"
-cd .vim/bundle/vim-powerline/; git checkout develop
 
 # Mercurial
 hg onsub --ignore-errors "hg pull --update"
