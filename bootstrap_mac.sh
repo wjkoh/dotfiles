f655b393c210e14e926ccae9ff6d409d835f5ca0
Woojong Koh <wjngkoh@gmail.com>
2019-04-12 17:49:25 -0700

    Remove .vim directory

--- bootstrap_mac.sh
+++ bootstrap_mac.sh
@@ -1,18 +1,14 @@
 #!/bin/sh
 
+brew install getantibody/tap/antibody
 brew tap caskroom/fonts
 brew cask install font-fira-code
-
-brew install getantibody/tap/antibody
 
 # Install both Python 2 and Python 3.
 # Note: Use python3 venv instead of virtualenv.
 brew install python@2 python
-
 brew install terminal-notifier
-
 brew install tmux
-git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
 
 # TODO: Install dracula iterm2 theme.
 brew install vim
