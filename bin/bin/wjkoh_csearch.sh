eb3e4606fa0a3c608c597128a70b2f38746947a6
Woojong Koh <wjngkoh@gmail.com>
2019-05-09 11:59:32 -0700

    Add comments to wjkoh_csearch.sh

--- bin/bin/wjkoh_csearch.sh
+++ bin/bin/wjkoh_csearch.sh
@@ -1,6 +1,8 @@
 #!/bin/sh
 
-# TODO(wjkoh): Enable syntax highlighting for code preview.
+# TODO(wjkoh): Return only a filename when exit.
+# TODO(wjkoh): Enable syntax highlighting for code preview. For example, extract
+# a file extension and give it to bat or other highlighters.
 csearch --color=always --context=5 -nostats --alphasort=false -n \
   "$@" \
   | sed 's/^File \(.*$\)/\x0\1:/g; s/^=\+$//g' \
