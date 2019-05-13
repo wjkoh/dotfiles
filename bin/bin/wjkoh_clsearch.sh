e17426752ca038cbcc894ffb007dca2af4b0a9c4
Woojong Koh <wjngkoh@gmail.com>
2019-05-13 11:26:59 -0700

    Update wjkoh_clsearch.sh

--- bin/bin/wjkoh_clsearch.sh
+++ bin/bin/wjkoh_clsearch.sh
@@ -1,9 +1,16 @@
 #!/bin/sh
 
 LAST_SATURDAY=$(date +"%Y-%m-%d" -d "last saturday")
+REMOVE_HTTP='s/^http:\/\///; '
+PREPEND_SUBMITTED='s/^/* Submitted: /; '
+PREPEND_UNDER_REVIEW='s/^/* Under review: /; '
+REPLACE_SINGLE_QUOTES='s/\x27\(.*\)\x27/`\1`/g; '
+KEEP_ONLY_FIRST_LINE='s/  .*`/`/'
+
+clsearch -desc_length=100 author:$USER is:submitted from:$LAST_SATURDAY | cut -d\  -f 1,6- \
+  | sed "$REMOVE_HTTP $PREPEND_SUBMITTED $REPLACE_SINGLE_QUOTES $KEEP_ONLY_FIRST_LINE"
 
 # Do not use from:... for pending CLs because some can be from very old.
 # Submitted CLs are okay ecuase from: detects created or submitted dates.
-# TODO(wjkoh): ; s/\'\(.*$\)\'/\1/
-clsearch -desc_length=100 author:$USER is:submitted from:$LAST_SATURDAY | cut -d\  -f 1,6- | sed 's/^http:\/\///; s/^/* Submitted: /; s/  .*$//'
-clsearch -desc_length=100 author:$USER is:pending reviewer:.+ | cut -d\  -f 1,7- | sed 's/^http:\/\///; s/^/* Under review: /; s/  .*$//'
+clsearch -desc_length=100 author:$USER is:pending reviewer:.+ | cut -d\  -f 1,7- \
+  | sed "$REMOVE_HTTP $PREPEND_UNDER_REVIEW $REPLACE_SINGLE_QUOTES $KEEP_ONLY_FIRST_LINE"
