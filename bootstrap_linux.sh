56b19350c87ccde4c1aa8d359590c7ea3e766b64
Woojong Koh <wjngkoh@gmail.com>
2019-06-10 01:41:02 -0700

    Install The Silver Searcher in bootstrap_linux.sh

--- bootstrap_linux.sh
+++ bootstrap_linux.sh
@@ -1,9 +1,13 @@
 #!/bin/bash
 ./pre_bootstrap.sh
 
+# Update the package index files first.
+sudo apt update
+
 curl -sL git.io/antibody | sh -s
 sudo apt install fd-find
 sudo apt install python-pip
+sudo apt install silversearcher-ag
 sudo apt install stow
 
 ./post_bootstrap.sh
