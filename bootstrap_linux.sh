3c90893de50d43f5a10c093928979e518b96509c
Woojong Koh <wjngkoh@gmail.com>
2019-06-18 13:21:11 -0700

    Install htop

--- bootstrap_linux.sh
+++ bootstrap_linux.sh
@@ -6,9 +6,10 @@ sudo apt update
 
 curl -sfL git.io/antibody | sh -s - -b /usr/local/bin || exit
 sudo apt install fd-find
+sudo apt install htop
 sudo apt install nodejs
-sudo apt install python3-pip
 sudo apt install python-pip
+sudo apt install python3-pip
 sudo apt install silversearcher-ag
 sudo apt install stow
 sudo apt install urlview
