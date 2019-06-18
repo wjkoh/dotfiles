410ce2cd9ac2761aaad8242b24a665d8a0ec6adc
Woojong Koh <wjngkoh@gmail.com>
2019-06-18 13:18:10 -0700

    Install nodejs, python3-pip, and all Tmux plugins

--- bootstrap_linux.sh
+++ bootstrap_linux.sh
@@ -4,10 +4,13 @@
 # Update the package index files first.
 sudo apt update
 
-curl -sL git.io/antibody | sh -s
+curl -sfL git.io/antibody | sh -s - -b /usr/local/bin || exit
 sudo apt install fd-find
+sudo apt install nodejs
+sudo apt install python3-pip
 sudo apt install python-pip
 sudo apt install silversearcher-ag
 sudo apt install stow
+sudo apt install urlview
 
 ./post_bootstrap.sh
