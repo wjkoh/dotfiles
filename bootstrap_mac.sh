fd33b28398dde00697c8152f9bf724b610ff4bdb
Woojong Koh <wjngkoh@gmail.com>
2019-07-09 19:31:01 -0700

    Install kitty on MacOS and Linux

--- bootstrap_mac.sh
+++ bootstrap_mac.sh
@@ -4,16 +4,10 @@
 # Update the homebrew formulae first.
 brew update
 
-echo 'Use Fira Code Regular 13pt'
-brew install getantibody/tap/antibody
-brew tap caskroom/fonts
-brew cask install font-fira-code
-
 # Install both Python 2 and Python 3.
 # Note: Use python3 venv instead of virtualenv.
 brew install python@2 python
 
-# TODO: Install dracula iterm2 theme.
 brew install clang-format
 brew install fd
 brew install hg
@@ -28,6 +22,12 @@ brew install the_silver_searcher
 brew install tmux
 brew install urlview
 brew install vim
+
+echo 'Use Fira Code Regular 11pt or 13pt.'
+brew install getantibody/tap/antibody
+brew tap caskroom/fonts
+brew cask install font-fira-code
+brew cask install kitty
 
 echo 'If you use ITerm2'
 echo 'Q: How do I make the option/alt key act like Meta or send escape codes?'
